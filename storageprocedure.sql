fa778026321033783da31cf749ed799f7e90396c
BryhanS <80595094+BryhanS@users.noreply.github.com>
2023-12-07 10:47:46 +0000

    terminado stored procedure

--- storageprocedure.sql
+++ storageprocedure.sql
@@ -8,3 +8,22 @@ begin
 end//
 
 call sp_get_documentos
+
+create procedure sp_get_ciudad_top(in top int)
+begin
+	/* store procede para ver consolidado de todas las ventas por ciudad de top, ingresar cero para tener todos los distritos */
+    
+    if top = 0 then
+		select ciudad,sum(monto) from IphoneData.detalle_ventas
+		group by ciudad
+		order by sum(monto) desc;
+	else
+		select ciudad,sum(monto) from IphoneData.detalle_ventas
+		group by ciudad
+		order by sum(monto) desc
+		limit top;
+    end if;
+    
+end//
+
+call sp_get_ciudad_top(5)
